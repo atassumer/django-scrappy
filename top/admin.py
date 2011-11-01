aa23405bddc921d6764664b8ed7bdf20e7c5b7be
teo <teo.sibileau@gmail.com>
2011-11-01 14:01:46 -0200

    Adding fixtures, some admin adjst

--- top/admin.py
+++ top/admin.py
@@ -2,4 +2,24 @@ from django.contrib.gis import admin
 
 from rstonetop500.top.models import *
 
-admin.site.register(TopAlbum)
+class TopAlbumAdmin(admin.ModelAdmin):
+	list_display=('position','had_it','title','band')
+	list_filter=('had_it',)
+	search_fields=('album__name','album__band__name')
+	actions=['had_it','dont_had_it']
+	def title(self,obj):
+		return obj.album.name
+	
+	def band(self,obj):
+		return obj.album.band
+	
+	def had_it(self,request,queryset):
+		queryset.update(had_it=True)
+
+	def dont_had_it(self,request,queryset):
+		queryset.update(had_it=False)
+
+	had_it.short_description='I had this album/s'
+	dont_had_it.short_description='I Don\'t had this album/s'
+
+admin.site.register(TopAlbum,TopAlbumAdmin)
--- top/models.py
+++ top/models.py
@@ -2,9 +2,12 @@ from django.contrib.gis.db import models
 from rstonetop500.albums.models import Album
 
 class TopAlbum(models.Model):
-  album=models.OneToOneField(Album)
-  position=models.PositiveIntegerField()
-  def __unicode__(self):
-    return u"%s | %s"%(self.position, self.album)
+	album=models.OneToOneField(Album)
+	position=models.PositiveIntegerField()
+	had_it=models.BooleanField(default=False)
+	class Meta:
+		ordering=('position',)
+	def __unicode__(self):
+		return u"%s | %s"%(self.position, self.album)
   
 
